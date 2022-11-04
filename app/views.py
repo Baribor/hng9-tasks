b5dccb6840b89d728a28623b9271b3d36f20779a
Baribor Saturday <saturdaybaribor@gmail.com>
2022-11-04 20:36:44 +0100

    refactor the endloint to work with both form data and application/json header catch some edge cases.

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import os
 import openai
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
-
+import json
 
 operation_types = {'multiplicatio n':'*', 'addition':'+', 'subtraction':'-'} 
 
@@ -11,8 +11,10 @@ openai.api_key = os.environ.get("API_KEY")
 
 @csrf_exempt
 def get_evaluation(request: HttpRequest):
-    
-    payload = request.POST
+    if request.POST:
+        payload = request.POST
+    else:
+         payload = json.loads(request.body.decode('utf-8'))
      
     result = None
     operation_type = None
@@ -25,7 +27,9 @@ def get_evaluation(request: HttpRequest):
   
 
     else:
-    
+        
+        synonyms = {'product':'multiplication', 'difference':'subtraction', 'sum':'addition'}
+        
         response = openai.Completion.create(
             model="text-davinci-002",
             prompt=f"{payload_ot} \n\n| solution | operation_type |",
@@ -36,6 +40,7 @@ def get_evaluation(request: HttpRequest):
             presence_penalty=0
         )
         _, result, operation_type, _=[x.strip() for x in  [y for y in response.choices[0].text.split('\n') if y][-1].split('|')]
+        operation_type = operation_type if not operation_type in synonyms else synonyms[operation_type]
       
     
     return JsonResponse({"slackUsername":"BarryDee", "result":int(result) ,"operation_type":operation_type})
